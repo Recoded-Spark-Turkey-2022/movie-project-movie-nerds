9677b04f6f2b11480244dcc99d7f69297748e8c2
nour al maleh <nourmale7@gmail.com>
2022-11-18 21:28:21 +0300

    genres is done and some cards using bootstrap

--- script.js
+++ script.js
@@ -3,12 +3,15 @@
 const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 const PROFILE_BASE_URL = "http://image.tmdb.org/t/p/w185";
 const BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780";
+const API_KEY = 'api_key=NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=';
+const searchUrl = (search) => {
+  return `${TMDB_BASE_URL}/search/multi?api_key=${atob('NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=')}&query=${search}`;
+}
+
 
 const CONTAINER = document.querySelector(".container");
-const home = document.getElementById("home")
-const genersNav = document.getElementById("genres")
-const actors = document.getElementById("actors")
-const searchInput = document.getElementById("search")
+
+
 
 
 // Don't touch this function please
@@ -17,13 +20,20 @@ const autorun = async () => {
   renderMovies(movies.results);
 };
 
-
 // Don't touch this function please
 const constructUrl = (path) => {
   return `${TMDB_BASE_URL}/${path}?api_key=${atob(
     "NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI="
   )}`;
 };
+
+
+const constructGenreUrl = (genreId) => {
+  return `${TMDB_BASE_URL}/discover/movie?api_key=${atob(
+    "NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI="
+  )}&with_genres=${genreId}`;
+}
+
 
 // You may need to add to this function, definitely don't delete it.
 const movieDetails = async (movie) => {
@@ -39,8 +49,6 @@ const fetchMovies = async () => {
 };
 
 
-
-
 // Don't touch this function please. This function is to fetch one movie.
 const fetchMovie = async (movieId) => {
   const url = constructUrl(`movie/${movieId}`);
@@ -49,118 +57,50 @@ const fetchMovie = async (movieId) => {
 };
 
 
-
-// //NOUR fetch releaseDates
-// const releaseDates = async (movieId) => {
-//   const url = constructUrl(`movie/${movieId}/release_dates`);
-//   const res = await fetch(url);
-//   return res.json();
-// };
-
-
-// //NOUR  fetch toprated
-// const topRated= async () => {
-//   const url = constructUrl(`movie/top_rated`);
-//   const res = await fetch(url);
-//   return res.json();
-// };
-
-// //NOUR fetch toprated
-// const upComing= async () => {
-//   const url = constructUrl(`movie/upcoming`);
-//   const res = await fetch(url);
-//   return res.json();
-// };
-
-
-
-//NOUR fetch actors
-const fetchActors = async () => {
-  const url = constructUrl(`person/popular`);
-  const res = await fetch(url);
-  const data = await res.json()
-  return data.results
-}
-
-//NOUR fetch one actor 
-const fetchActor = async (actorId) => {
-  const url = constructUrl(`person/${actorId}`);
-  const res = await fetch(url);
-  const data = await res.json()
-  return data.results
-}
-
-
-  
-
-
 // You'll need to play with this function in order to add features and enhance the style.
 const renderMovies = (movies) => {
   CONTAINER.innerHTML= " "
   movies.map((movie) => {
     const movieDiv = document.createElement("div");
-    movieDiv.innerHTML = `
-        <img src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${
-      movie.title
-    } poster" >
-        <h3>${movie.title}</h3>`;
+    movieDiv.className = "card"
+    movieDiv.innerHTML = `<div id = "card" class="card text-bg-dark" ">
+    <img "img-fluid rounded-start" class="card text-bg-dark" id="card-img-top" src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${movie.title} poster" style=" object-fit: cover;">
+     <div class="card-body text-center" id = "card-body"> 
+       <h5 class="card-title">${movie.title}</h5>
+        <h6 class="card-text > " <span> Rating </span> <br> ${movie.vote_average} </br> </h5>
+      </div>
+    </div>`;
     movieDiv.addEventListener("click", () => {
       movieDetails(movie);
     });
     CONTAINER.appendChild(movieDiv);
   });
 };
-
 
 
 // You'll need to play with this function in order to add features and enhance the style.
 const renderMovie = (movie) => {
   CONTAINER.innerHTML = `
-    <div class="row">
-        <div class="col-md-4">
-             <img id="movie-backdrop" src=${
-               BACKDROP_BASE_URL + movie.backdrop_path
-             }>
-        </div>
-        <div class="col-md-8">
-            <h2 id="movie-title">${movie.title}</h2>
-            <p id="movie-release-date"><b>Release Date:</b> ${
-              movie.release_date
-            }</p>
-            <p id="movie-runtime"><b>Runtime:</b> ${movie.runtime} Minutes</p>
-            <h3>Overview:</h3>
-            <p id="movie-overview">${movie.overview}</p>
-        </div>
-        </div>
-            <h3>Actors:</h3>
-            <ul id="actors" class="list-unstyled"></ul>
-    </div>`;
-};
-
-
-const renderfilter = (movies) => {
-  movies.map((movie) => {
-    const movieDiv = document.createElement("div");
-    movieDiv.innerHTML = `
-        <img src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${
-      movie.title
-    } poster" >
-        <h3>${movie.title}</h3>`;
-    movieDiv.addEventListener("click", () => {
-      movieDetails(movie);
-    });
-    CONTAINER.appendChild(movieDiv);
-  });
-};
+<div class="card mb-3"  >
+  <img src=${
+    BACKDROP_BASE_URL + movie.backdrop_path
+  } class="card-img-top" text-bg-dark" alt="...">
+  <div class="card-body">
+  <h3 class="card-title" >${movie.title}</h3>
+    <p id="movie-release-date" class="card-title" ><b>Release Date:</b> ${
+      movie.release_date
+    }</p>
+    <p id="movie-runtime" class="card-title" ><b>Runtime:</b> ${movie.runtime} Minutes</p>
+    <h3>Overview:</h3>
+    <p id="movie-overview" class="card-title" >${movie.overview}</p>
+  </div>`};
 
 
 document.addEventListener("DOMContentLoaded", autorun);
 
 
+
 // NOUR filtering 
-
-const filtersNav = document.getElementById("filter")
-
 // popular
 const popular= document.getElementById("popular")
 
@@ -171,8 +111,6 @@ const filterPopular = async () => {
   renderMovies(movies.results);
 }
 popular.addEventListener("click", filterPopular)
-
-
 
 // toprated
 const toprated= document.getElementById("topRated")
@@ -185,8 +123,6 @@ const filtertoprated= async () => {
 }
 toprated.addEventListener("click", filtertoprated )
 
-
-
 // upComing
 const upComing= document.getElementById("upComing")
 
@@ -197,7 +133,6 @@ const filterupcoming= async () => {
   renderMovies(movies.results);
 }
 upComing.addEventListener("click", filterupcoming )
-
 
 // nowPlaying
 const nowPlaying= document.getElementById("nowPlaying")
@@ -212,15 +147,44 @@ nowPlaying.addEventListener("click", filternowPlaying )
 
 
 
+//genres
+// fetching list of movies for one genre
+const moviefiltering = async (id) =>{
+  const url = constructGenreUrl(id);
+  const res = await fetch(url);
+  const movies = await res.json();
+  renderMovies(movies.results);
+}
+
+// NOUR Function to fetch data from API
+const filtergenres= async () => {
+  const url = constructUrl(`genre/movie/list`);
+  console.log(url);
+  const res = await fetch(url);
+  const data = await res.json();
+  return data['genres']
+}
+const genresnavbar = async () =>{
+const genres = await filtergenres()
+const genreslist = document.getElementById("genres")
+for (let genre in genres) {
+  const li = document.createElement("li")
+  li.classList.add("dropdown-item")
+  li.innerHTML = genres[genre]['name']
+  genreslist.appendChild(li)
+
+  //add event listener to each geners
+  li.addEventListener("click", () =>{
+    moviefiltering(genres[genre]['id'])
+  })}}
+ genresnavbar();
+
+
 
 // NOUR homepage refresh function 
 function refreshPage(){
   window.location.reload();
 } 
 
-function goBackAndRefresh() {
-  window.history.go(-1);
-  setTimeout(() => {
-    location.reload();
-  }, 0);
-}
+
+  
